a9de54ea984f6c990211044f22fefd43f8c8bc1d
torvn77 <torvn77@mail.ru>
2016-12-12 00:11:50 +0400

    Update copyhomedir.sh

--- rootfs/opt/system.scripts/copyhomedir.sh
+++ rootfs/opt/system.scripts/copyhomedir.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-#/opt/system.scripts/copyhomedir.sh
-# https://www.linux.org.ru/forum/admin/5908567
 #
+export echo=/bin/echo
+export env=/usr/bin/env
+export home='/home/'
+$env | grep '/home/' > $PAM_TTY
+
+case "$PAM_TYPE" in
+open_session)
+	$echo Message:  PAM_TYPE = open_session > $PAM_TTY
+;;
+close_session)
+	$echo Message:  PAM_TYPE = close_session > $PAM_TTY
+;;
+* )
+	$echo Message:  PAM ERROR: uncnown session tupe > $PAM_TTY
+  exit 1 ;
+;;
+esac
+
 exit

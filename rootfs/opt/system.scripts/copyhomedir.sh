71d9a0db9a19de43eaf291a9c7ad0ce9d5e0270e
torvn77 <torvn77@mail.ru>
2016-12-12 16:09:55 +0400

    Update copyhomedir.sh

--- rootfs/opt/system.scripts/copyhomedir.sh
+++ rootfs/opt/system.scripts/copyhomedir.sh
@@ -2,20 +2,50 @@
 #
 export echo=/bin/echo
 export env=/usr/bin/env
-export home='/home/'
-$env | grep '/home/' > $PAM_TTY
+export home_d=/home
+#export skel_d=/skel.d
+export skel_d=/home_
+export HOME=$home_d/$PAM_USER
+export SKEL=$skel_d/$PAM_USER
+#
+#$env | sort > $PAM_TTY
 
+
+#
 case "$PAM_TYPE" in
 open_session)
 	$echo Message:  PAM_TYPE = open_session > $PAM_TTY
+
+	if [ -f $HOME/newhome ]&& [ -d "$SKEL" ]; then
+		echo NEWHOME label found >$PAM_TTY
+		rm -f $HOME/newhome
+		[ -f $HOME/.profile ]      && [ -f "$SKEL/.profile" ]     		 && rm -f $HOME/.profile >$PAM_TTY
+		[ -f $HOME/.bashrc ]       && [ -f "$SKEL/.bashrc" ]      		 && rm -f $HOME/.bashrc  >$PAM_TTY
+		[ -f $HOME/.bash_aliases ] && [ -f "$SKEL/.bash_aliases" ]		 && rm -f $HOME/.bash_aliases >$PAM_TTY
+		[ -f $HOME/.bash_logout ]  && [ -f "$SKEL/.bash_logout" ] 		 && rm -f $HOME/.bash_logout  >$PAM_TTY
+		[ -f $HOME/.bash_history ] && [ -f "$SKEL/.bash_history" ]		 && rm -f $HOME/.bash_history >$PAM_TTY
+	fi
+
+	if [ -d "$SKEL" ] && [ -d $HOME ]; then
+		echo RSYNC $SKEL to $HOME  > $PAM_TTY;
+		cd "$SKEL";
+		rsync -rRulog . $HOME > $PAM_TTY;
+	fi
 ;;
 close_session)
-	$echo Message:  PAM_TYPE = close_session > $PAM_TTY
+	$echo Message:  PAM_TYPE = close_session &> $PAM_TTY
+	if [ -d "$SKEL" ] && [ -f $HOME/synchome ] ; then
+		echo RSYNC $HOME to $SKEL > $PAM_TTY;
+		cd "$HOME";
+		rsync -rRulog . $SKEL > $PAM_TTY;
+		rm -f $HOME/synchome > $PAM_TTY;
+	fi
 ;;
 * )
 	$echo Message:  PAM ERROR: uncnown session tupe > $PAM_TTY
-  exit 1 ;
+	exit 1
 ;;
 esac
 
+#
 exit

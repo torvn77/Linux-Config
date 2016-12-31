3b583f84d9a951684b535852fd6fb55a123d3ee3
torvn77 <torvn77@mail.ru>
2017-01-01 03:32:18 +0400

    Update and rename rootfs/root/ARC/import_key.sh to rootfs/root/Scripts/import_key.sh

---
+++ rootfs/root/Scripts/import_key.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# /root/Scripts/import-key.sh
+
+# linuxcnc.org key
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 3CB9FD148F374FEF
+# buildbot.linuxcnc.org key
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-key EF1B07FEE0EE663E
+
+# Tox key
+#wget -qO - https://pkg.tox.chat/debian/pkg.gpg.key | apt-key add -
+#wget -qO - http://download.opensuse.org/repositories/home:antonbatenev:tox/Debian_Stretch/Release.key | apt-key add -
+#wget -qO - http://download.opensuse.org/repositories/home:antonbatenev:tox/Debian_8.0/Release.key | apt-key add -
+
+# "Launchpad PPA for FreeCAD maintainers"
+# https://launchpad.net/~freecad-maintainers/+archive/ubuntu/freecad-daily
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 005EAE8119BB5BCA
+
+# deb     http://www.deb-multimedia.org stretch main non-free
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 5C808C2B65558117
+
+apt-get install apt-transport-https
+apt-get update
